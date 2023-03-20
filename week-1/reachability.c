46b582fdb38de553314b1582931421b8cdd311e8
ilker m. sitki <ilkermeliksitki@proton.me>
2023-03-20 13:04:50 +0300

    add first draft of reachability

---
+++ week-1/reachability.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int vertex;
+    struct node *next;
+};
+
+struct node **create_adj_list(int n, int m);
+
+int main(void)
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    struct node **adj_list = create_adj_list(n , m);
+    return 0;
+}
+
+struct node **create_adj_list(int n, int m)
+{
+    struct node **adj_list = malloc(sizeof(struct node *) * n + 1);
+    for (int i = 0; i < n + 1; ++i) {
+        adj_list[i] = NULL;
+    }
+    int a, b;
+    for (int i = 0; i < m; ++i) {
+        scanf("%d %d", &a, &b);
+        struct node *n = malloc(sizeof(struct node));
+        n->vertex = a;
+        n->next = NULL;
+        if (adj_list[a] == NULL) {
+            adj_list[a] = n;
+        } else {
+            struct node *second_node = adj_list[a]->next;
+            n->next = second_node;
+            adj_list[a]->next = n;
+        }
+    }
+    return adj_list;
+}
